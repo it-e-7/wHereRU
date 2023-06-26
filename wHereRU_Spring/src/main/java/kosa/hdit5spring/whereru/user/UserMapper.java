f2e4adfbb0ed739ee6c08dd3757c09a1192fa775
qmdl980 <qmdl98056@gmail.com>
2023-06-26 11:18:00 +0900

    struct: 스프링 프로젝트 생성

---
+++ wHereRU_Spring/src/main/java/kosa/hdit5spring/whereru/user/UserMapper.java
@@ -0,0 +1,10 @@
+package kosa.hdit5spring.whereru.user;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Mapper
+public interface UserMapper {
+
+}
