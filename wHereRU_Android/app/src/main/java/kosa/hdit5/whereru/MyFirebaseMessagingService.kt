53cb0c666ddf179bb53869181847500512efcc53
DESKTOP-J84SQJK\KOSA <yjh9099@gmail.com>
2023-06-27 14:26:46 +0900

    [feat] 알림기능-안드로이드와 FCM연동

---
+++ wHereRU_Android/app/src/main/java/kosa/hdit5/whereru/MyFirebaseMessagingService.kt
@@ -0,0 +1,12 @@
+package kosa.hdit5.whereru
+
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    override fun onNewToken(token: String) {
+        Log.d("토큰","$token")
+    }
+
+}
