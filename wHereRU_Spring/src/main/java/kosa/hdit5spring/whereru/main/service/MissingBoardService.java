0e108d647f1fc3a434c63cd4189e964e1cb3e131
youmin <super_vet@naver.com>
2023-06-27 10:23:44 +0900

    [FEAT] : [0627] 글쓰기

---
+++ wHereRU_Spring/src/main/java/kosa/hdit5spring/whereru/main/service/MissingBoardService.java
@@ -0,0 +1,10 @@
+package kosa.hdit5spring.whereru.main.service;
+
+import kosa.hdit5spring.whereru.main.vo.MissingBoardVo;
+
+public interface MissingBoardService {
+	
+	void writeMissingBoard(MissingBoardVo missingBoardVo);
+
+	
+}
---
+++ wHereRU_Spring/src/main/java/kosa/hdit5spring/whereru/main/vo/MissingBoardVo.java
@@ -0,0 +1,20 @@
+package kosa.hdit5spring.whereru.main.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MissingBoardVo {
+
+	private int missingSeq;
+	private String missingName;
+	private int missingAge;
+	private String missingSex;
+	private String missingOutfit;
+	private String missingTime;
+	private String missingPoint;
+	private int userSeq;
+}
