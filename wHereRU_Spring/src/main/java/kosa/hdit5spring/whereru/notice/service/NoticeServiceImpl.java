46d6f4db2512e86b927c7fdffd949057dc1e56d6
DESKTOP-J84SQJK\KOSA <yjh9099@gmail.com>
2023-06-28 17:54:43 +0900

    [FEAT] 알림기능-서버구현_23.06.28

--- wHereRU_Spring/src/main/java/kosa/hdit5spring/whereru/notice/service/NoticeServiceImpl.java
+++ wHereRU_Spring/src/main/java/kosa/hdit5spring/whereru/notice/service/NoticeServiceImpl.java
@@ -6,14 +6,21 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class NoticeServiceImpl implements NoticeService {
 
 	@Override
 	public void requestToFCM(List<String> tokenList) {
-		
+
 		
 		for(String token : tokenList) {
 			 try {
+				 	System.out.println(token);
 		            String url = "https://fcm.googleapis.com/fcm/send";
 
 		            // FCM 서버 키
@@ -23,8 +30,8 @@ public class NoticeServiceImpl implements NoticeService {
 		            String targetToken = token;
 
 		            // 메시지 데이터
-		            String message = "{ \"to\": \"" + targetToken + "\", \"data\": { \"title\": \"유민이에게\", \"body\": \"안녕..\" } }";
-
+		            String message = "{ \"to\": \"" + targetToken + "\",\"priority\": \"high\", \"notification\": { \"title\": \"HI!!\", \"body\": \"Can you look at this??\" } }";
+		            System.out.println(message);
 		            URL obj = new URL(url);
 		            HttpURLConnection conn = (HttpURLConnection) obj.openConnection();
 
@@ -44,7 +51,7 @@ public class NoticeServiceImpl implements NoticeService {
 
 		            int responseCode = conn.getResponseCode();
 		            System.out.println("Response Code: " + responseCode);
-
+		            
 		            if (responseCode == HttpURLConnection.HTTP_OK) {
 		                //성공시
 		            } else {
